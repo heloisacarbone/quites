0e2291ace542617c1ff882583354cb3eb3d2c6cf
heloisacarbone <heloisacarbone@gmail.com>
2016-11-19 19:30:12 -0200

    teste

---
+++ tests/user-action-test.js
@@ -0,0 +1,27 @@
+
+var assert = require('assert');
+var database = require('../backend/database_controller.js')
+
+describe('Registration and Login', function() {
+	before(function (done) {
+        database.initialize(function () {
+            database['users'].add("admin", "admin", "admin", "admin", "2016-10-05","male", function(err, obj){
+              database['users'].add("devel", "devel", "devel", "devel", "2016-10-05","male", function(err, obj){          
+              });
+            
+            });
+            done();
+        });
+    });
+  	it('it should search for a user', function() {
+   
+   		database['users'].getSearchUsers("devel", function(err, obj){
+
+    		  assert.ifError(err);
+        	assert.ok(!obj);
+            
+    	});
+ 
+  	});
+
+});
